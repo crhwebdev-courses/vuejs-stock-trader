cc5321a9cfa2da1625bd62379bd2d019dbe38c60
Carl Hain <crhain@gmail.com>
2020-04-01 13:23:15 -0500

    Add dummy data to Stocks component

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,7 +1,20 @@
 <template>
   <div>
-    <h1>The Stocks Component</h1>
+    <div></div>
   </div>
 </template>
 
-<script></script>
+<script>
+export default {
+  data() {
+    return {
+      stocks: [
+        { id: 1, name: "BMW", price: 110 },
+        { id: 2, name: "Google", price: 200 },
+        { id: 3, name: "Apple", price: 250 },
+        { id: 4, name: "Twitter", price: 8 }
+      ]
+    };
+  }
+};
+</script>
